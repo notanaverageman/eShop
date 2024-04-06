969aabfafd9e64079a941f7682b6bca66871fcf5
Yusuf Tarık Günaydın <yusuf_tarik@hotmail.com>
2024-04-06 13:01:36 +0300

    Enable Prometheus metrics endpoint.

--- src/eShop.ServiceDefaults/Extensions.cs
+++ src/eShop.ServiceDefaults/Extensions.cs
@@ -117,7 +117,7 @@ public static partial class Extensions
                         .WithMetrics(metrics =>
                         {
                             // Uncomment the following line to enable the Prometheus endpoint
-                            //metrics.AddPrometheusExporter();
+                            metrics.AddPrometheusExporter();
                         });
 
         return builder;
@@ -141,7 +141,7 @@ public static partial class Extensions
     public static WebApplication MapDefaultEndpoints(this WebApplication app)
     {
         // Uncomment the following line to enable the Prometheus endpoint (requires the OpenTelemetry.Exporter.Prometheus.AspNetCore package)
-        // app.MapPrometheusScrapingEndpoint();
+        app.MapPrometheusScrapingEndpoint();
 
         // All health checks must pass for app to be considered ready to accept traffic after starting
         app.MapHealthChecks("/health");
